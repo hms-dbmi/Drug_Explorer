b63c76fdac7a7dd1fa5dae6a36b99ab433d9e05d
wangqianwen0418 <wangqianwen0418@163.com>
2020-08-18 12:47:25 -0400

    load drug graph

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,8 +1,11 @@
 import * as React from "react"
+import * as d3 from "d3"
+import axios from "axios"
 
 interface Props {
     height: number,
-    width: number
+    width: number,
+    selectedDrugID:string
 }
 
 interface State {
@@ -10,6 +13,21 @@ interface State {
 }
 
 export default class Model extends React.Component<Props, State>{
+    getDrugPaths(){
+        const drugJson = './data/drug_graph.json'
+        axios.get(drugJson)
+            .then(res => {
+                let response = res.data
+
+                Object.keys(response).forEach(drugID=>{
+                    console.info(drugID, response[drugID].paths.length)
+                })
+                console.info(response)
+            })
+    }
+    componentDidMount(){
+        this.getDrugPaths()
+    }
     render(){
         return <div className='model'>
             Model
