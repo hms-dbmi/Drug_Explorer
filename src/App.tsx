efda43ba5446731698527d48e1081f1683894b93
wangqianwen0418 <wangqianwen0418@163.com>
2023-03-08 12:15:01 -0500

    chore: update about info

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PathMatrix from 'components/CardContainer';
 import NodeLink from 'components/TabContainer';
 import DrugSider from 'components/Sider';
 
-import { Layout } from 'antd';
+import { Layout, Modal } from 'antd';
 import { InfoCircleOutlined, LinkOutlined } from '@ant-design/icons';
 import './App.css';
 import { StateConsumer } from 'stores';
@@ -26,6 +26,7 @@ interface Props {
 interface State {
   width: number;
   height: number;
+  isModalVisible: boolean;
 }
 
 class App extends React.Component<Props, State> {
@@ -34,10 +35,19 @@ class App extends React.Component<Props, State> {
     this.state = {
       width: window.innerWidth,
       height: window.innerHeight,
+      isModalVisible: false,
     };
 
     this.updateSize = this.updateSize.bind(this);
   }
+
+  showModal = () => {
+    this.setState({ isModalVisible: true });
+  };
+
+  hideModal = () => {
+    this.setState({ isModalVisible: false });
+  };
 
   updateSize() {
     this.setState({
@@ -100,14 +110,16 @@ class App extends React.Component<Props, State> {
           style={{ height: headerHeight * 0.8, padding: '2px 10px' }}
           alt="logo"
         />
-        TxGNN Explorer
-        <span style={{ float: 'right' }}>
-          <InfoCircleOutlined style={{ height: headerHeight }} />
+        <b style={{ fontSize: '30px' }}>TxGNN Explorer</b>
+        <span style={{ float: 'right' }} onClick={this.showModal}>
+          <InfoCircleOutlined
+            style={{ height: headerHeight, fontSize: '20px' }}
+          />
           <span
             style={{
-              fontSize: '20px',
+              paddingLeft: '5px',
+              fontSize: '18px',
               height: headerHeight,
-              marginLeft: '4px',
             }}
           >
             About
@@ -117,34 +129,92 @@ class App extends React.Component<Props, State> {
     );
 
     return (
-      <Layout>
-        {header}
-
+      <>
         <Layout>
-          <DrugSider siderWidth={siderWidth} />
-          <Content className="main" style={{ height: mainViewHeight }}>
-            <PathMatrix width={mainViewWidth} height={MatrixHeight} />
-            <NodeLink width={mainViewWidth} height={NodeLinkHeight} />
-          </Content>
+          {header}
+
+          <Layout>
+            <DrugSider siderWidth={siderWidth} />
+            <Content className="main" style={{ height: mainViewHeight }}>
+              <PathMatrix width={mainViewWidth} height={MatrixHeight} />
+              <NodeLink width={mainViewWidth} height={NodeLinkHeight} />
+            </Content>
+          </Layout>
+          <Footer
+            style={{
+              textAlign: 'center',
+              color: 'gray',
+              height: footHeight,
+              padding: 0,
+            }}
+          >
+            Copyright © {new Date().getFullYear()} Harvard.{' '}
+            <a href="http://gehlenborglab.org/" target="_blank_">
+              Gehlenborg Lab <LinkOutlined />
+            </a>{' '}
+            &{' '}
+            <a href="https://zitniklab.hms.harvard.edu/" target="_blank_">
+              Zitnik Lab <LinkOutlined />
+            </a>
+          </Footer>
         </Layout>
-        <Footer
-          style={{
-            textAlign: 'center',
-            color: 'gray',
-            height: footHeight,
-            padding: 0,
-          }}
+
+        <Modal
+          title="About TxGNN Explorer"
+          visible={this.state.isModalVisible}
+          footer={null}
+          onCancel={this.hideModal}
+          zIndex={1099}
         >
-          Copyright © {new Date().getFullYear()} Harvard.{' '}
-          <a href="http://gehlenborglab.org/" target="_blank_">
-            Gehlenborg Lab <LinkOutlined />
-          </a>{' '}
-          &{' '}
-          <a href="https://zitniklab.hms.harvard.edu/" target="_blank_">
-            Zitnik Lab <LinkOutlined />
+          <h3>About</h3>
+          <p>
+            TxGNN Explorer provides a visual interface to interact with the
+            predicitons and explanations of TXGNN, a model for identifying
+            therapeutic opportunities for diseases with limited treatment
+            options and minimal molecular understanding.
+          </p>
+          <p>
+            TxGNN is a graph neural network pre-trained on a comprehensive
+            knowledge graph of 17,080 clinically-recognized diseases and 7,957
+            therapeutic candidates. The model can process various therapeutic
+            tasks in a unified formulation. Once trained, we show that TXGNN can
+            perform zero-shot inference on new diseases without the need for
+            additional parameters or fine-tuning on ground truth labels.
+          </p>
+
+          <p>
+            The design of TxGNN Explorer follows a user-centered design method.
+            Please refer to our IEEE VIS 2022{' '}
+            <a
+              href="https://ieeexplore.ieee.org/document/9916585"
+              target="_blank_"
+            >
+              paper
+            </a>{' '}
+            (honorable mention) for more details about the design,
+            implementation, and evaluation of TxGNN Explorer.
+          </p>
+          <h3>Team</h3>
+          <p>
+            Kexin Huang<sup>*</sup>, Payal Chandak<sup>*</sup>, Qianwen Wang,
+            Shreyas Havaldar, Akhil Vaid, Jure Leskovec, Girish Nadkarni,
+            Benjamin S. Glicksberg, Nils Gehlenborg, and Marinka Zitnik
+            <h4>Contact: marinka@hms.harvard.edu</h4>
+          </p>
+
+          <h3>More</h3>
+          <a href="https://github.com/mims-harvard/TxGNN" target="_blank">
+            Github Repo: TxGNN <LinkOutlined />
           </a>
-        </Footer>
-      </Layout>
+          <br />
+          <a
+            href="https://github.com/hms-dbmi/drug_explorer/tree/TxGNN"
+            target="_blank"
+          >
+            Github Repo: TxGNN Explorer <LinkOutlined />
+          </a>
+        </Modal>
+      </>
     );
   }
 }
